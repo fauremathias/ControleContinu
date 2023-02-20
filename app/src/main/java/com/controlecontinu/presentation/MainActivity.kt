c67460d0c88d99231f6aa9037d52b7e4c132fb7e
Mathias <mathias124@gmail.com>
2023-02-20 09:28:17 +0100

    marche toujours pas

--- app/src/main/java/com/controlecontinu/presentation/MainActivity.kt
+++ app/src/main/java/com/controlecontinu/presentation/MainActivity.kt
@@ -51,9 +51,20 @@ class MainActivity : AppCompatActivity(), ITodoListener, IRefreshTodoListener{
             setTitle("Add task")
             setPositiveButton("Add") { _, _ ->
                 try {
-                    val todo = viewModel.createTodo(editText.text.toString())
-                    viewModel.saveTodo(this@MainActivity, todo)
-                    adapter.notifyDataSetChanged()
+                    var isValid = true
+
+                    if ( editText.text.toString().isEmpty() ) {
+                        isValid = false
+                        Toast.makeText(this@MainActivity, "empty", Toast.LENGTH_SHORT).show()
+                    }
+
+                    if ( isValid ) {
+                        val todo = viewModel.createTodo(editText.text.toString())
+                        viewModel.saveTodo(this@MainActivity, todo)
+                        adapter.notifyDataSetChanged()
+                    }
+
+
                 } catch (e: java.lang.Exception) {
                     Toast.makeText(this@MainActivity, "Failed task", Toast.LENGTH_SHORT).show()
                 }
